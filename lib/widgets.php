dcbd8bf03d1ded34793e67f0bd66c3737547df10
George Holmes <george@georgeholmesii.com>
2016-01-25 14:29:15 -0600

    Added right sidebar template SCSS

--- lib/widgets.php
+++ lib/widgets.php
@@ -5,8 +5,8 @@ if (function_exists('register_sidebar'))
 {
     // Define Sidebar Widget Area 1
     register_sidebar(array(
-        'name' => __('Sidebar Widgets', 'fission'),
-        'description' => __('Sidebar widget area...', 'fission'),
+        'name' => __('Sidebar Widgets', 'trunck'),
+        'description' => __('Sidebar widget area...', 'trunck'),
         'id' => 'sidebar-widget-area',
         'before_widget' => '<div id="%1$s" class="%2$s">',
         'after_widget' => '</div>',
@@ -16,8 +16,8 @@ if (function_exists('register_sidebar'))
 
     // Define Sidebar Widget Area 2
     register_sidebar(array(
-        'name' => __('Footer Widgets', 'fission'),
-        'description' => __('Footer widget area...', 'fission'),
+        'name' => __('Footer Widgets', 'trunck'),
+        'description' => __('Footer widget area...', 'trunck'),
         'id' => 'footer-widget-area',
         'before_widget' => '<div id="%1$s" class="%2$s columns medium-4">',
         'after_widget' => '</div>',
